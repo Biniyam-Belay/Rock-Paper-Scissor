09ebe82f85dc271f3cb8a6c0e8e71131a0463f94
Biniyam Belay <binibelaygom@gmail.com>
2023-11-12 16:23:17 +0300

    rock apper sccissor

--- js/script.js
+++ js/script.js
@@ -1,78 +1,55 @@
-const word = ["rock", "paper", "scissor"];
-
-// A function that generates computerSelection randomly.
+// Function to get the computer's choice
 function getComputerChoice() {
-  const randomNum = Math.floor(Math.random() * 3); // generate random number between 0 and 2
-  if (randomNum === 0) {
-    return 'Rock';
-  } else if (randomNum === 1) {
-    return 'Paper';
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
+}
+
+// Function to play a single round
+function playRound(playerSelection, computerSelection) {
+  // Make playerSelection case-insensitive
+  playerSelection = playerSelection.toLowerCase();
+
+  // Determine the winner
+  if (playerSelection === computerSelection.toLowerCase()) {
+      return 'It\'s a tie!';
+  } else if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'rock') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+      return `You Win! ${playerSelection} beats ${computerSelection}.`;
   } else {
-    return 'Scissors';
+      return `You Lose! ${computerSelection} beats ${playerSelection}.`;
   }
 }
 
-// A function to accept player and computer Selection five times.
-
-function game() {
+// Function to play the game
+function playGame() {
   let playerScore = 0;
   let computerScore = 0;
 
   for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt('Choose Rock, Paper, or Scissors:').toLowerCase();
-    const computerSelection = getComputerChoice();
-    const result = playround(playerSelection, computerSelection);
-    console.log(result);
+      const playerChoice = prompt('Enter Rock, Paper, or Scissors:');
+      const computerChoice = getComputerChoice();
 
-    if (result.startsWith('You Win')) {
-      playerScore++;
-    } else if (result.startsWith('You Lose')) {
-      computerScore++;
-    }
+      const result = playRound(playerChoice, computerChoice);
+      console.log(result);
+
+      if (result.includes('Win')) {
+          playerScore++;
+      } else if (result.includes('Lose')) {
+          computerScore++;
+      }
   }
 
-  if (playerScore > computerScore) {
-    console.log(`You win the game! Final score: ${playerScore} to ${computerScore}`);
-  } else if (playerScore < computerScore) {
-    console.log(`You lose the game! Final score: ${playerScore} to ${computerScore}`);
-  } else {
-    console.log(`It's a tie! Final score: ${playerScore} to ${computerScore}`);
-  }
+  // Display the final result
+  const finalResult =
+      playerScore > computerScore
+          ? 'Congratulations! You win the game.'
+          : playerScore < computerScore
+          ? 'Sorry, you lose the game.'
+          : 'It\'s a tie!';
+
+  document.getElementById('result').innerText = finalResult;
 }
-
-
-
-/* A function that takes the player and the computer selection. 
-    check the scores for the player and the computer
-*/
-function playround(playerSelection, computerSelection) {
-  if (playerSelection === "rock") {
-    if (computerSelection === "Paper") {
-      return "You Lose! Paper beats Rock";
-    } else if (computerSelection === "Scissors") {
-      return "You Win! Rock beats Scissors";
-    } else {
-      return "It's a tie!";
-    }
-  } else if (playerSelection === "paper") {
-    if (computerSelection === "Scissors") {
-      return "You Lose! Scissors beats Paper";
-    } else if (computerSelection === "Rock") {
-      return "You Win! Paper beats Rock";
-    } else {
-      return "It's a tie!";
-    }
-  } else if (playerSelection === "scissors") {
-    if (computerSelection === "Rock") {
-      return "You Lose! Rock beats Scissors";
-    } else if (computerSelection === "Paper") {
-      return "You Win! Scissors beats Paper";
-    } else {
-      return "It's a tie!";
-    }
-  } else {
-    return "Invalid input! Please choose either Rock, Paper, or Scissors.";
-  }
-}
-playround()
-game()
