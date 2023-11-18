1ad8bb0a0b0d7d5263df2c7937add945084a3efc
Biniyam Belay <binibelaygom@gmail.com>
2023-11-19 01:22:18 +0300

    worked on the ui of rps

--- js/script.js
+++ js/script.js
@@ -5,51 +5,66 @@ function getComputerChoice() {
   return choices[randomIndex];
 }
 
+let playerScore = 0
+let computerScore = 0
+
 // Function to play a single round
 function playRound(playerSelection, computerSelection) {
   // Make playerSelection case-insensitive
   playerSelection = playerSelection.toLowerCase();
+  console.log(playerSelection, computerSelection)
 
   // Determine the winner
   if (playerSelection === computerSelection.toLowerCase()) {
-      return 'It\'s a tie!';
+      console.log( 'It\'s a tie!');
   } else if (
       (playerSelection === 'rock' && computerSelection === 'scissors') ||
       (playerSelection === 'paper' && computerSelection === 'rock') ||
       (playerSelection === 'scissors' && computerSelection === 'paper')
   ) {
-      return `You Win! ${playerSelection} beats ${computerSelection}.`;
+      console.log( `You Win! ${playerSelection} beats ${computerSelection}.`);
+
+      playerScore += 1
+      const scorep = document.querySelector('#scorep')
+      scorep.textContent = `Player Score : ${playerScore}`
   } else {
-      return `You Lose! ${computerSelection} beats ${playerSelection}.`;
-  }
-}
+      console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`);
 
-// Function to play the game
-function playGame() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-      const playerChoice = prompt('Enter Rock, Paper, or Scissors:');
-      const computerChoice = getComputerChoice();
-
-      const result = playRound(playerChoice, computerChoice);
-      console.log(result);
-
-      if (result.includes('Win')) {
-          playerScore++;
-      } else if (result.includes('Lose')) {
-          computerScore++;
-      }
+      computerScore += 1
+      const scorec = document.querySelector('#scorec')
+      scorec.textContent = `Computer Score : ${computerScore}`
   }
 
-  // Display the final result
-  const finalResult =
-      playerScore > computerScore
-          ? 'Congratulations! You win the game.'
-          : playerScore < computerScore
-          ? 'Sorry, you lose the game.'
-          : 'It\'s a tie!';
 
-  document.getElementById('result').innerText = finalResult;
+//Winner check
+// FIXME: The code needs to stop after the plaer or the computer wins the game. It's not doing that now.
+
+  if (playerScore === 5){
+    const winnerp = document.querySelector('#winner');
+    winnerp.textContent = `The Winner is : you`
+  }
+  if (computerScore === 5){
+    const winnerp = document.querySelector('#winner');
+    winnerp.textContent = `The Winner is : The Computer`
+  }
+
 }
+
+//buttons of respective rock paper or scissor
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  playRound("rock", computerSelection.toLowerCase());
+})
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  playRound("paper", computerSelection.toLowerCase());
+})
+
+const scissor = document.querySelector('#scissors');
+scissor.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  playRound("scissors", computerSelection.toLowerCase());
+})
